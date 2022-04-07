df3b06a3257de567d2ab6e8988d42a603dd40635
jrwilbur <101979301+jrwilbur@users.noreply.github.com>
2022-04-07 12:41:43 -0400

    Add files via upload

---
+++ test/jar-ui/src/App.js
@@ -0,0 +1,22 @@
+import React from 'react'
+import { BrowserRouter, Routes, Route, Link } from 'react-router-dom';
+
+import LandingPage from './components/pages/LandingPage';
+import LoginPage from './components/pages/LoginPage';
+import RegisterPage from './components/pages/RegisterPage';
+import ForgetPasswordPage from './components/pages/ForgetPasswordPage';
+import HomePage from './components/pages/HomePage';
+import Footer from './components/pages/Footer';
+import './App.css';
+import Login from './components/pages/Login';
+
+function App() {
+  return (
+    <Login >
+      <Footer />
+    </Login>
+
+  );
+}
+
+export default App;
