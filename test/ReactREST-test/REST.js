853bad9e2e91ab2650236285aa11c48593871a64
jrwilbur <101979301+jrwilbur@users.noreply.github.com>
2022-04-08 21:23:01 -0400

    Create REST.js
    This is a react REST example

---
+++ test/ReactREST-test/REST.js
@@ -0,0 +1,29 @@
+//<!---React REST with Functional Component--->
+import React from 'react'
+import { StyleSheet, Text, View } from 'react-native'
+
+export default function ￼() {
+	return (
+		<View>
+			<Text>￼</Text>
+		</View>
+	)
+}
+
+const styles = StyleSheet.create({})
+
+//<!---React REST with Functional Component and Export--->
+import React from 'react'
+import { StyleSheet, Text, View } from 'react-native'
+
+const ￼ = () => {
+	return (
+		<View>
+			<Text>￼</Text>
+		</View>
+	)
+}
+
+export default ￼
+
+const styles = StyleSheet.create({})
